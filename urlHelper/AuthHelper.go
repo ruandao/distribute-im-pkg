94a907fdf397d8f03583bef8ef01dfa1a97feb47
ruandao <ljy080829@gmail.com>
2025-08-23 23:02:55 +0800

    urlHelper.Auth

---
+++ urlHelper/AuthHelper.go
@@ -0,0 +1,49 @@
+package urlhelper
+
+import "fmt"
+
+type AuthHelper struct {
+	host string
+}
+
+func Auth(hostList ...string) *AuthHelper {
+	if len(hostList) > 1 {
+		panic("only allow zero or one host")
+	}
+	var authHelper *AuthHelper
+	if len(hostList) == 0 {
+		authHelper = &AuthHelper{host: ""}
+	} else {
+		authHelper = &AuthHelper{host: hostList[0]}
+	}
+	return authHelper
+}
+func (helper *AuthHelper) Register() string {
+	return fmt.Sprintf("%v/register", helper.host)
+}
+func (helper *AuthHelper) MultipleCreateUser() string {
+	return fmt.Sprintf("%v/multipleCreateUser", helper.host)
+}
+func (helper *AuthHelper) BatchCreateUser() string {
+	return fmt.Sprintf("%v/batchCreateUser", helper.host)
+}
+func (helper *AuthHelper) Login() string {
+	return fmt.Sprintf("%v/login", helper.host)
+}
+
+func (helper *AuthHelper) QueryUser() string {
+	return fmt.Sprintf("%v/queryUser", helper.host)
+}
+
+func (helper *AuthHelper) GetNotLoginRandomUser() string {
+	return fmt.Sprintf("%v/getNotLoginRandomUser", helper.host)
+}
+func (helper *AuthHelper) ReInitNotLoginSet() string {
+	return fmt.Sprintf("%v/reInitNotLoginSet", helper.host)
+}
+func (helper *AuthHelper) CometAddr() string {
+	return fmt.Sprintf("%v/cometAddr", helper.host)
+}
+func (helper *AuthHelper) Helloword() string {
+	return fmt.Sprintf("%v/helloword", helper.host)
+}
