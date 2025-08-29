9092c1a45b83c7e909292e58d0de7870e65b5fff
ruandao <ljy080829@gmail.com>
2025-08-29 16:05:52 +0800

    调整 TickerRun 为 RunWithTickerDuration

--- lib/TickerRun.go
+++ lib/TickerRun.go
@@ -5,15 +5,57 @@ import (
 	"time"
 )
 
-func TickerRun(ctx context.Context, ticker *time.Ticker, ticketF func() bool) (context.Context, context.CancelFunc) {
+func RunWithTicker(ctx context.Context, ticker time.Ticker, taskF func() bool) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
+
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case <-ticker.C:
-				goon := ticketF()
+				goon := taskF()
+				if !goon {
+					cancel()
+					return
+				}
+			}
+		}
+	}()
+	return ctx, cancel
+}
+func RunWithTickerDuration(ctx context.Context, tickerDuration time.Duration, taskF func() bool) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(ctx)
+	ticker := time.NewTicker(tickerDuration)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				goon := taskF()
+				if !goon {
+					cancel()
+					ticker.Stop()
+					return
+				}
+			}
+		}
+	}()
+	return ctx, cancel
+}
+
+func RunWithCancel(ctx context.Context, taskF func() bool) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				goon := taskF()
 				if !goon {
 					cancel()
 					return
