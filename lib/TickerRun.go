b2da9e411086d22e67ab4606442daeea26cfcb44
ruandao <ljy080829@gmail.com>
2025-08-29 18:49:23 +0800

    封装 for 循环,确保每次循环都会打印一个日志

--- lib/TickerRun.go
+++ lib/TickerRun.go
@@ -3,65 +3,76 @@ package lib
 import (
 	"context"
 	"time"
+
+	"github.com/ruandao/distribute-im-pkg/lib/logx"
 )
+
+func RunForever(ctx context.Context, name string, taskF func() bool) {
+	for {
+		logx.DebugX(name)()
+		goon := taskF()
+		if !goon {
+			return
+		}
+	}
+}
 
 func RunWithTicker(ctx context.Context, ticker time.Ticker, taskF func() bool) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				goon := taskF()
-				if !goon {
-					cancel()
-					return
-				}
+	go RunForever(ctx, "RunWithTicker", func() bool {
+		select {
+		case <-ctx.Done():
+			return false
+		case <-ticker.C:
+			goon := taskF()
+			if !goon {
+				cancel()
+				return false
 			}
 		}
-	}()
+		return true
+	})
+
 	return ctx, cancel
 }
 func RunWithTickerDuration(ctx context.Context, tickerDuration time.Duration, taskF func() bool) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 	ticker := time.NewTicker(tickerDuration)
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-ticker.C:
-				goon := taskF()
-				if !goon {
-					cancel()
-					ticker.Stop()
-					return
-				}
+	go RunForever(ctx, "RunWithTickerDuration", func() bool {
+		select {
+		case <-ctx.Done():
+			return false
+		case <-ticker.C:
+			goon := taskF()
+			if !goon {
+				cancel()
+				ticker.Stop()
+				return false
 			}
 		}
-	}()
+		return true
+	})
+
 	return ctx, cancel
 }
 
 func RunWithCancel(ctx context.Context, taskF func() bool) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				goon := taskF()
-				if !goon {
-					cancel()
-					return
-				}
+	go RunForever(ctx, "RunWithCancel", func() bool {
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+			goon := taskF()
+			if !goon {
+				cancel()
+				return false
 			}
 		}
-	}()
+		return true
+	})
 	return ctx, cancel
 }
